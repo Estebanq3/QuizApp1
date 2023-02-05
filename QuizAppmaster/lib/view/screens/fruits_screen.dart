201fa6725047c184d311cfe213ba3d6ac393de49
Estebanq3 <estebanq52@gmail.com>
2023-02-05 17:37:44 -0600

    Paneles de vocabulario de frutas implementados

--- QuizAppmaster/lib/view/screens/fruits_screen.dart
+++ QuizAppmaster/lib/view/screens/fruits_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:quizapp2/view/screens/start_screen.dart';
+import 'package:quizapp2/view/screens/vocabulary_screen.dart';
 import 'package:slimy_card/slimy_card.dart';
 import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 
@@ -16,7 +17,7 @@ class FruitsPage extends StatelessWidget {
       child: Column(
         children: [
           Image.asset(
-            'Assets/watermelon.png',
+            'Assets/Fruits/watermelon.png',
             width: 200,
             height: 200,
           ),
@@ -29,37 +30,616 @@ class FruitsPage extends StatelessWidget {
               fontWeight: FontWeight.w700,
             ),
           ),
+          Text(
+            "Sandía",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
         ],
       ),
     ),
     Container(
       alignment: Alignment.center,
       color: Colors.red,
-      child: const Text('2'),
+      child: Column(
+        children: [
+          Image.asset(
+            'Assets/Fruits/cherry.png',
+            width: 200,
+            height: 200,
+          ),
+          Text(
+            "Cherry",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Text(
+            "Cereza",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    ),
+    Container(
+      alignment: Alignment.center,
+      color: Colors.green,
+      child: Column(
+        children: [
+          Image.asset(
+            'Assets/Fruits/apple.png',
+            width: 200,
+            height: 200,
+          ),
+          Text(
+            "Apple",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Text(
+            "Manzana",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    ),
+    Container(
+      alignment: Alignment.center,
+      color: Colors.pink,
+      child: Column(
+        children: [
+          Image.asset(
+            'Assets/Fruits/apricot.png',
+            width: 200,
+            height: 200,
+          ),
+          Text(
+            "Apricot",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Text(
+            "Albaricoque",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    ),
+    Container(
+      alignment: Alignment.center,
+      color: Colors.lightGreen,
+      child: Column(
+        children: [
+          Image.asset(
+            'Assets/Fruits/avocado.png',
+            width: 200,
+            height: 200,
+          ),
+          Text(
+            "Avocado",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Text(
+            "Aguacate",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    ),
+    Container(
+      alignment: Alignment.center,
+      color: Colors.yellow,
+      child: Column(
+        children: [
+          Image.asset(
+            'Assets/Fruits/banana.png',
+            width: 200,
+            height: 200,
+          ),
+          Text(
+            "Banana",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Text(
+            "Banana",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
     ),
     Container(
       alignment: Alignment.center,
       color: Colors.purple,
-      child: const Text('3'),
-    )
+      child: Column(
+        children: [
+          Image.asset(
+            'Assets/Fruits/blackberry.png',
+            width: 200,
+            height: 200,
+          ),
+          Text(
+            "Blackberry",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Text(
+            "Mora",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    ),
+    Container(
+      alignment: Alignment.center,
+      color: Colors.indigo,
+      child: Column(
+        children: [
+          Image.asset(
+            'Assets/Fruits/cantaloupe.png',
+            width: 200,
+            height: 200,
+          ),
+          Text(
+            "Cantaloupe",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Text(
+            "Melón",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    ),
+    Container(
+      alignment: Alignment.center,
+      color: Colors.deepPurple,
+      child: Column(
+        children: [
+          Image.asset(
+            'Assets/Fruits/grapes.png',
+            width: 200,
+            height: 200,
+          ),
+          Text(
+            "Grapes",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Text(
+            "Uvas",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    ),
+    Container(
+      alignment: Alignment.center,
+      color: Colors.lightGreen,
+      child: Column(
+        children: [
+          Image.asset(
+            'Assets/Fruits/kiwi.png',
+            width: 200,
+            height: 200,
+          ),
+          Text(
+            "Kiwi",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Text(
+            "Kiwi",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    ),
+    Container(
+      alignment: Alignment.center,
+      color: Colors.lightBlue,
+      child: Column(
+        children: [
+          Image.asset(
+            'Assets/Fruits/lemon.png',
+            width: 200,
+            height: 200,
+          ),
+          Text(
+            "Lemon",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Text(
+            "Limón",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    ),
+    Container(
+      alignment: Alignment.center,
+      color: Colors.redAccent,
+      child: Column(
+        children: [
+          Image.asset(
+            'Assets/Fruits/lime.png',
+            width: 200,
+            height: 200,
+          ),
+          Text(
+            "Lime",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Text(
+            "Lima",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    ),
+    Container(
+      alignment: Alignment.center,
+      color: Colors.orange,
+      child: Column(
+        children: [
+          Image.asset(
+            'Assets/Fruits/mango.png',
+            width: 200,
+            height: 200,
+          ),
+          Text(
+            "Mango",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Text(
+            "Mango",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    ),
+    Container(
+      alignment: Alignment.center,
+      color: Colors.lime,
+      child: Column(
+        children: [
+          Image.asset(
+            'Assets/Fruits/orange.png',
+            width: 200,
+            height: 200,
+          ),
+          Text(
+            "Orange",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Text(
+            "Naranja",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    ),
+    Container(
+      alignment: Alignment.center,
+      color: Colors.pinkAccent,
+      child: Column(
+        children: [
+          Image.asset(
+            'Assets/Fruits/papaya.png',
+            width: 200,
+            height: 200,
+          ),
+          Text(
+            "Papaya",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Text(
+            "Papaya",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    ),
+    Container(
+      alignment: Alignment.center,
+      color: Colors.grey,
+      child: Column(
+        children: [
+          Image.asset(
+            'Assets/Fruits/peach.png',
+            width: 200,
+            height: 200,
+          ),
+          Text(
+            "Peach",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Text(
+            "Melocotón",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    ),
+    Container(
+      alignment: Alignment.center,
+      color: Colors.green,
+      child: Column(
+        children: [
+          Image.asset(
+            'Assets/Fruits/pineaple.png',
+            width: 200,
+            height: 200,
+          ),
+          Text(
+            "Pineapple",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Text(
+            "Piña",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    ),
+    Container(
+      alignment: Alignment.center,
+      color: Colors.tealAccent,
+      child: Column(
+        children: [
+          Image.asset(
+            'Assets/Fruits/strawberry.png',
+            width: 200,
+            height: 200,
+          ),
+          Text(
+            "Strawberry",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Text(
+            "Fresa",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    ),
+    Container(
+      alignment: Alignment.center,
+      color: Colors.green,
+      child: Column(
+        children: [
+          Image.asset(
+            'Assets/Fruits/apple.png',
+            width: 200,
+            height: 200,
+          ),
+          Text(
+            "Apple",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Text(
+            "Manzana",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.mulish(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    ),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
-      alignment: Alignment.center,
-      child: Container(
-        width: 200,
-        height: 320,
-        color: Colors.lightBlueAccent,
-        child: DefaultTextStyle(
-          style: const TextStyle(),
-          child: CardSwiper(
-            cards: cards,
-          ),
-        ),
-      ),
-    );
+        color: Colors.white,
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 300,
+              height: 350,
+              color: Colors.lightBlueAccent,
+              child: DefaultTextStyle(
+                style: const TextStyle(),
+                child: CardSwiper(
+                  cards: cards,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+              width: 10,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => const ThirdPage()));
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+              ),
+              child: Image.asset(
+                'Assets/return.png',
+                width: 100,
+                height: 50,
+              ),
+            )
+          ],
+        ));
   }
 }
